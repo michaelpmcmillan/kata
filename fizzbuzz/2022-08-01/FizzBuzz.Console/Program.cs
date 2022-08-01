a88e4b470a35241f7bf9127146b09e7d4e1d8eb5
Mike McMillan <mike.mcmillan@onthebeach.co.uk>
2022-08-01 13:23:26 +0100

    Add FizzBuzz.Console

---
+++ fizzbuzz/2022-08-01/FizzBuzz.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FizzBuzz;
+
+Console.WriteLine("Hello, World!");
+
+var itemGenerator = new FizzBuzzItemGenerator();
+
+foreach(var item in new FizzBuzzResultGenerator(itemGenerator).Generate(1, 100))
+{
+    Console.WriteLine(item.ToString());
+}
