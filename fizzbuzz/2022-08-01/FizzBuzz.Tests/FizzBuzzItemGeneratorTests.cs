d3f1552d32262175bdd7e448c6c5627f31a32e9c
Mike McMillan <mike.mcmillan@onthebeach.co.uk>
2022-08-01 13:28:04 +0100

    Add Buzz and FizzBuzz

--- fizzbuzz/2022-08-01/FizzBuzz.Tests/FizzBuzzItemGeneratorTests.cs
+++ fizzbuzz/2022-08-01/FizzBuzz.Tests/FizzBuzzItemGeneratorTests.cs
@@ -14,12 +14,18 @@ namespace FizzBuzz.Tests
         [InlineData(2, "2")]
         [InlineData(3, "Fizz")]
         [InlineData(4, "4")]
-        [InlineData(5, "5")]
+        [InlineData(5, "Buzz")]
         [InlineData(6, "Fizz")]
         [InlineData(7, "7")]
         [InlineData(8, "8")]
         [InlineData(9, "Fizz")]
-        [InlineData(10, "10")]
+        [InlineData(10, "Buzz")]
+        [InlineData(11, "11")]
+        [InlineData(12, "Fizz")]
+        [InlineData(13, "13")]
+        [InlineData(14, "14")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(16, "16")]
         public void GivenAllFizzNumbersBetween1and100_WhenGetItem_ReturnFizzForEachItem(int number, string expectation)
         {
             var subject = new FizzBuzzItemGenerator();
--- fizzbuzz/2022-08-01/FizzBuzz/FizzBuzzItemGenerator.cs
+++ fizzbuzz/2022-08-01/FizzBuzz/FizzBuzzItemGenerator.cs
@@ -4,8 +4,12 @@ public class FizzBuzzItemGenerator : IFizzBuzzItemGenerator
 {
     public string GetItem(int number)
     {
-        return number % 3 == 0
-            ? "Fizz"
-            : number.ToString();
+        var result = GetFizz(number) + GetBuzz(number);
+        return result == ""
+            ? number.ToString()
+            : result;
     }
+
+    private string GetFizz(int number) => number % 3 == 0 ? "Fizz" : "";
+    private string GetBuzz(int number) => number % 5 == 0 ? "Buzz" : "";
 }
